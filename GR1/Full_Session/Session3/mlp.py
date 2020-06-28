140d4dc6b846b2336e73608a484bd825eda8f1a6
diepdap5 <diepdap5@gmail.com>
2020-06-28 23:21:11 +0700

    update session, fix some small syntax bug

--- GR1/Full_Session/Session3/mlp.py
+++ GR1/Full_Session/Session3/mlp.py
@@ -1,6 +1,7 @@
+import tensorflow as tf
 class MLP:
 	def __init__(self, vocab_size, hidden_size):
-		delf._vocab_size = vocab_size
+		self._vocab_size = vocab_size
 		self._hidden_size = hidden_size
 	def build_graph(self):
 		self._X = tf.placeholder(tf.float32, shape=[None, self._vocab_size])
@@ -48,10 +49,76 @@ class MLP:
 	def trainer(self, loss, learning_rate):
 		train_op = tf.train.AdamOptimizer(learning_rate).minimize(loss)
 		return train_op
+class DataReader:
+	def __init__(self, data_path, batch_size, vocab_size):
+		self._batch_size = batch_size
+		with open(data_path) as f:
+			d_lines = f.read().splitlines()
 
+		self._data = []
+		self._labels = []
+		for data_id, line in enumerate(d_lines):
+			vector = [0.0 for _ in range(vocab_size)]
+			features = line.split('<fff>')
+			label, doc_id = int(features[0]), int(features[1])
+			tokens = features[2].split()
+			for tokens in tokens:
+				index, value = int(token.split(':')[0]), float(token.split(':')[1])
+				vector[index] = value
+			self._data.append(vector)
+			self._labels.append(label)
+		self._data = np.array(self._data)
+		self._labels = np.array(self._labels)
 
+		self._num_epoch = 0
+		self._batch_id =0	
+	def next_batch(self):
+		start = self._batch_id * self._batch_size
+		end = start + self._batch_size
+		self._batch_id += 1
+
+		if end + self._batch_size > len(self._data):
+			end = len(self._data)
+			self._num_epoch += 1
+			self._batch_id = 0
+			indices = range(len(self._data))
+			random.seed(indices)
+			self._data, self._labels = self._data[indices], self._labels[indices]
+		return self._data[start:end], self._labels[start:end]	
+def load_dataset():
+	train_data_reader = DataReader(
+		data_path='../datasets/20news-train-tfidf.txt',
+		batch_size=50,
+		vocab_size=vocab_size
+	)
+	test_data_reader = DataReader(
+		data_path='../datasets/20news-test-tfidf.txt',
+		batch_size=50,
+		vocab_size=vocab_size
+	)
+	return train_data_reader, test_data_reader
+
+def save_parameters(name, value, epoch):
+	filename = name.replace(':', '-colon-') + '-epoch-{}.txt'.format(epoch)
+	if len(value.shape) == 1:
+		string_form = ',',join([str(number) for number in value])
+	else:
+		string_form = '\n'.join([','.join([str(number) for number in value[row]]) for row in range(value.shape[0])])
+	with open('../saved-paras/' + filename, 'w') as f:
+		f.write(string_form)
+
+def restore_parameters(name, epoch):
+	filename = name.replace(':', '-colon-') + '-epoch-{}.txt'.format(epoch)
+	with open('../saved-paras/' + filename) as f:
+		lines = f.read().splitlines()
+	if len(lines) == 1:
+		value = [float(number) for number in lines[0].split(',')]
+	else:
+		value = [[float(number) for number in lines[0].split(',')] for row in range(len(lines))]
+		return value
 if __name__ == '__main__':
-	with open(../datasets/words_idfs.txt) as f:
+
+	with open('../datasets/20news-bydate/words_idfs.txt') as f:
 		vocab_size = len(f.read().splitlines())
 
 	mlp = MLP(
@@ -63,5 +130,51 @@ if __name__ == '__main__':
 	with tf.Session() as sess:
 		train_data_reader, test_data_reader = load_dataset()
 		step, MAX_STEP = 0, 1000 ** 2
+
+		sess.run(tf.global_variables_initializer())
+		while step < MAX_STEP:
+			train_data, train_labels = train_data_reader.next_batch()
+			plabels_eval, loss_eval, _ = sess.run(
+				[predicted_labels, loss, train_op],
+				feed_dict={
+					mlp._X: train_data,
+					mlp._real_Y: train_labels
+				}
+			)
+			step += 1
+			print("step: {}, loss: {}".format(step, loss_eval))
+		trainable_variables = tf.trainable_variables()
+		for variable in trainable_variables:
+			save_parameters(
+				name=variable.name,
+				value=variable.eval(),
+				epoch=train_data_reader._num_epoch
+			)
+	with tf.Session() as sess:
+		epoch = 10
+
+		trainable_variables = tf.trainable_variables()
+		for variable in trainable_variables:
+			saved_value = restore_parameters(variable.name, epoch)
+			assign_op = variable.assign(saved_value)
+			sess.run(assign_op)
+		num_true_preds = 0
+		while True:
+			test_data, test_labels = test_data_reader.next_batch()
+			test_plabels_eval = sess.run(
+				predicted_labels,
+				feed_dict={
+					mlp._X: test_data,
+					mlp._real_Y: test_labels
+				}
+			)
+			matches = np.equal(test_plabels_eval, test_labels)
+			num_true_preds += np.sum(matches.astype(float))
+
+			if test_data_reader._batch_id == 0:
+				break
+		print("Epoch:", epoch)
+		print("Accuracy on test data:", num_true_preds/ len(test_data_reader._data))
+
 
 
